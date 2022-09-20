2b5574a74a8f07d8a14cb5ae6f00e67cb7794525
aemolnar11 <amolnar1@jh.edu>
2022-09-20 13:19:07 -0400

    week2 code

---
+++ week2/needleman-wunsch/week2.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import sys
+from fasta import readFASTA 
+
+input_sequences = readFASTA(open(sys.argv[1]))
+
+##need to import these matrixes appropriately as an array so its readable 
+
+matrix = np.loadtxt(sys.argv[2], dtype ="object", skiprows=1)
+##system agrument is saying to work with the second file listed 
+header = list(matrix[:,0])
+scorematrx = matrix[:,1:].astype(float)
+
+# print(header)
+# exit()
+
+seq1_id, sequence1 = input_sequences[0]
+seq2_id, sequence2 = input_sequences[1]
+
+##we need to know how big our sequences are to make the array
+##we just make the array idk?
+
+F_matrix = np.zeros((len(sequence1)+1, len(sequence2)+1))
+Trace_matrix = np.zeros((len(sequence1)+1, len(sequence2)+1))
+
+# Now we need to fill in the values in the first row and
+# first column, based on the gap penalty. Let's fill in the
+# first column.
+
+gap_penalty = float(sys.argv[3])
+
+for i in range(len(sequence1)+1) :
+    F_matrix[i,0] = i * gap_penalty
+    Trace_matrix[i,0] = 3
+
+# Now fill in the first row
+
+for j in range(len(sequence2)+1) :
+    F_matrix[0,j] = j * gap_penalty
+    Trace_matrix[0,j] = 2
+
+#### d = 1
+#### h = 2
+#### v = 3
+    
+#print(F_matrix)
+#print(Trace_matrix)
+
+##the penalties for stuff
+
+# match_score = float(sys.argv[1])
+# ##turning sys.argv output into a float
+#
+# mismatch_score = float(sys.argv[2])
+#gap_penalty = float(sys.argv[3])
+##gap penalty should be a negative value 
+
+##now we populate the matrixes
+##we need to index the substitution matrix to figure out the match/mismatch score
+
+for i in range(1, len(sequence1)+1):
+    ##i is the header
+    for j in range(1, len(sequence2)+1):
+        ##j is the score
+        # n1 = sequence1[i-1]
+#         n2 = sequence2[j-1]
+        
+        # you dont have nucleotide for sequence 1
+        # you dont have nucleotide for sequence 2
+        # define n1 and n2
+        # find where in scorematrx n1 and n2 fall in 
+        # find d from scorematrx
+        # compute match/mismatch/gap value to define F_matrix[i,j]
+        index_i = header.index(sequence1[i-1])
+        index_j = header.index(sequence2[j-1])
+        ##needed to put n1 and n2 in index so we can get them 
+        d = F_matrix[i-1, j-1] + scorematrx[index_i, index_j]
+        ##adding scorematrx values based on our index for i and j
+        h = F_matrix[i, j-1] + gap_penalty
+        v = F_matrix[i-1, j] + gap_penalty
+        
+        F_matrix[i,j] = max(d,h,v)
+        ##when we run it for hw we will care
+        ##now our F matrix is populated
+        
+        ##now we need to populate the traceback matrix
+       
+       #### d = 1
+       #### h = 2
+       #### v = 3
+        
+        if d >= h or d >= v :
+            Trace_matrix[i,j] = 1
+        elif h > d or h >= v :
+            Trace_matrix[i,j] = 2
+        elif v > d or v > h :
+            Trace_matrix[i,j] = 3
+        ##this is populating the traceback matrix with either d h or v 
+        ##we need to fix this so that the sides are not only 0s because then we will stop at the wrong spot 
+        
+        
+#print(Trace_matrix)
+        
+        ##we want to iterate backwards up the matrix
+        ##can go up one and left one or 0
+        ##we can be 1 distance from where we were or -1 distance
+        
+for i, row in Trace_matrix[::-1] :
+    ##so this is specifying that we start from the bottom corner
+    
+
+ Trace_matrix.shape
+##idk what we need to do ummmmmm
+##need a while loop and do an elif for all the options of gettin h, d, or v
+##so like if its h, align1 += something, align2 += some stuff idk
+##we have to do this for all the options
+##i know while loops help us to edit the stuff without makign the loop confused
+
+
+print(Trace_matrix.shape)
+
+
+# while i > 0
+#
+#
+#
+#     if i == 0 :
+#         best_index = len(row)
+#     else :
+#         v_gap = best_index
+#         v_match = best_index - 1
+#         ## -1 makes it go up to the left from the corner
+#         if row[v_gap] < row[v_match] :
+#             best_index = v_match
+#             ##this selects for the matching over the gapping if the score is            the same
+#             ##best_index is us choosing to go left or not, its is our j
+#
+# ##how do we put this into the F_matrix?
+# ##how do we rebuild the sequence from this information
+# ##we need to align sequence 2 to sequence 1
+# ##we need to repopulate sequence 2 with the best options based on F matrix scores
+# ##we have to say the movement of diagnol or straight up equals its corresponding j sequence position
+# ##idk how to do this erhgjahd'piqwnd
+
