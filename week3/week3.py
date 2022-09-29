13a530217705afd3ce81d44210aefdb26a137c3d
aemolnar11 <amolnar1@jh.edu>
2022-09-29 15:54:26 -0400

    week3.py update

--- week3/week3.py
+++ week3/week3.py
@@ -7,7 +7,10 @@ import numpy as np
 
 from actualvcfParser import parse_vcf
 
+##import the stuff to use it
+
 Parsed_vcf = parse_vcf("sCerrevisae_final.vcf")
+##we already made a vcf parser so we use it 
 
 #print(Parsed_vcf[1])
 
@@ -16,13 +19,20 @@ allele_frequency = []
 predicted_effects = []
 depth = []
 quality = []
+##we need dictonaries to put the stuff in that we pull out from the vcf file
 
 for i in range(1,len(Parsed_vcf)) :
+    ##looking through each thing after the header in the Parsed_vcf file (our scerreviase file after its been parsed)
     allele_frequency.append(Parsed_vcf[i][7]["AF"])
+    ##the allele frequency is in the 7th thing in the list, and within that dictonary that makes up number 7 we want to pull out the value for AF
     annotation = Parsed_vcf[i][7]["ANN"]
+##we wanted to pull out anythin the value for ANN in the 7th list item which is that same dictonary 
     predicted_effects.append(annotation.split('|')[1])
+    ##we want to parse things by "|" (seperate everything by "|") we then want to take the 2nd value from that parsed list
     depth.append(Parsed_vcf[i][7]["DP"])
+    ##we are looking for the the value for the key DP in list 7 which is a dictonary
     quality.append(Parsed_vcf[i][7]["QA"])
+    ##looking for the value corresponding to key QA in list 7 which is a dictonary 
     
 #print(quality)
     
@@ -33,18 +43,23 @@ effects = {}
 ##is i in effects already?
 
 for i in predicted_effects :
+    ##going through each thning in our predicted effects list
     if i in effects.keys() :
         effects[i] += 1
+        ##if the effect is already in the dictonary, add +1 to the value interger for that key
     else: 
         effects[i] = 1
+        ##if that key does not yet exist in the effects dictonary, add the key and make the value 1
         
 #print(effects)
-     
     
 
 
+## we are now printing everything
+##describe which plot we need, label x and y axis and give it a title
 
 fig, axs = plt.subplots(2,2)
+##this set up allows us to make the setup for the panel
 
 axs[0,0].hist( (allele_frequency), density=True )
 axs[0,0].set_yscale('log')
@@ -77,6 +92,8 @@ axs[1,1].set_title("Read Depth Density")
 #fig.tight_layout()
 
 fig.tight_layout()
-plt.show()
+##this makes the graph not look bad
 
+plt.show()
+##we then show the plot
 
